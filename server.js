4ff5cecd9f9c1b928fc830b204b68eb5a490520e
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2022-05-29 16:54:20 +0530

    port changed

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req,res) => {
     res.send(`Hey there, welcome to my first deploy on aws, ${process.env.la}`)
 })
 
-app.listen(1000, () => {
+app.listen(8080, () => {
     console.log('Hey there, it is up!')
 })
